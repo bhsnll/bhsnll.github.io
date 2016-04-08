dbcfea69b33860fd950f4e817ac91ae5c057fd86
Braden Snell <braden.snell@byu.edu>
2016-04-08 16:13:04 -0600

    latest deploy

---
+++ angular-wrapper.js
@@ -0,0 +1,64 @@
+var app = angular.module("myApp", []);
+
+app.controller("myController", function($scope) {
+    $scope.date = new Date();
+    $scope.options = {
+        displayAs: "MM/DD/YY",
+        theme: {
+            primary: '#990000',
+            primary_text: '#ffffff',
+            secondary: '#ffffff',
+            secondary_text: '#444444',
+            secondary_accent: '#990000'
+        }
+    }
+});
+
+app.directive("datiumPicker", function($timeout) {
+    return {
+        restrict: 'A',
+        scope: {
+            options: '=datiumOptions',
+            ngModel: '='
+        },
+        require: '?ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            
+            var picker = new Datium(element[0], scope.options);
+            
+            scope.$watch('options', function(options) {
+                picker.updateOptions(options); 
+            }, true);
+            
+            if (ngModel === null) return;
+            
+            
+            var date = new Date(scope.ngModel);
+            $timeout(function() {
+                if (date.toString() === 'Invalid Date') {
+                    scope.ngModel = void 0;
+                } else {
+                    picker.setDate(date); 
+                    picker.setDefined();
+                }   
+            });
+            
+            ngModel.$options = {
+                updateOn: 'datium-viewchanged',
+                updateOnDefault: false
+            };
+            
+            ngModel.$formatters.unshift(function(value) {
+                var date = new Date(value);
+                if (date.toString() !== 'Invalid Date') {
+                    picker.setDate(date);
+                }
+                return picker.toString();
+            });
+            
+            ngModel.$parsers.unshift(function(value) {
+                return picker.getDate();
+            });
+        }
+    }
+});
